2dd8fc482d2022e16b3728bfa49a9f8e634d69ca
IVB107 <ianvb107@gmail.com>
2019-05-31 11:38:03 -0500

    Completed oop1.py

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,36 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base class:
+class Vehicle:
+    def __init__(self):
+        self.flight_vehicle = FlightVehicle()
+
+# [Vehicle] has a [FlightVehicle]
+class FlightVehicle:
+    def __init__(self):
+        self.starship = Starship()
+
+# [FlightVehicle] has a [Starship]
+class Starship:
+    pass
+
+# [Airplane] is a [FlightVehicle]
+class Airplane(FlightVehicle):
+    pass
+
+# [GroundVehicle] is a [Vehicle]
+class GroundVehicle(Vehicle):
+    pass
+
+# [Car] is a [GroundVehicle]
+class Car(GroundVehicle):
+    pass
+
+# [Motorcycle] is a [GroundVehicle]
+class Motorcycle(GroundVehicle):
+    pass
+
+
+
